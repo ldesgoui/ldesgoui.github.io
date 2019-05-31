a3a2f4d97579ed6aeef7c952a804302dc82bd6ca
ldesgoui <ldesgoui@gmail.com>
2019-05-31 19:23:02 +0200

    init other branch

---
+++ resume.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env nix-shell
+#!nix-shell --pure -i sh -p pandoc fira fira-mono texlive.combined.scheme-medium
+
+pandoc -s -f markdown -o resume.pdf --pdf-engine xelatex << END
+---
+title: Lucas Desgouilles
+subtitle: Software Developer
+mainfont: Fira Sans
+monofont: Fira Mono
+sansfont: Fira Sans
+documentclass: extarticle
+geometry:
+- top=20mm
+- bottom=0mm
+---
+
+-----
+<ldesgoui@gmail.com> | [+336 88 24 28 20](tel:+33688242820) | <https://github.com/ldesgoui>
+-----
+
+
+Seeking an opportunity to facilitate self-growth in a professional environment using Rust.
+
+
+# Skillset
+
+Programming
+: Rust, Python, Haskell, Elm, C
+
+Linux
+: deployment target, daily driver since 2014
+
+Autonomous workflow
+: git, kanban
+
+
+# Experience
+
+2016 - Now
+:   *Freelance Developer*
+
+    Web development using Python, Javascript, Elm, Haskell.
+    Development and deployment of solutions to help digitalization of companies
+    from various industries (yachting, driving school, restauration).
+    Administration of Linux servers (debian, CentOS, NixOS) and cloud infrastructure (AWS, GCP).
+
+2015 - Now
+:   *Contributor to Free Software*
+
+    Contributed self-documenting feature to [PostgREST](https://postgrest.org),
+    documentation to [\`specs\`](https://docs.rs/specs),
+    package definitions to [\`nixpkgs\`](https://nixos.org/nixpkgs).
+    Author of Rust library [\`discord_game_sdk\`](https://docs.rs/discord_game_sdk).
+    Licensed most side projects with MIT, Apache-2.0 or GPLv3.0.
+    Contributions can be found on <https://github.com/ldesgoui>.
+
+2015 - 2016 (6 months)
+:   *Computer Programming Tutor* [42 - Paris](https://42.fr)
+
+    Assisted students from different schools to self-teach basics of imperative programming
+    with Ruby in order to realize a small program at the end of two weeks.
+    3 classes: students (20-24) in Commerce, in Graphic Design, and girls (14-15) from a nearby school.
+
+2014 (2 months)
+:   *Intern Web Developer* [dotConferences](https://www.dotconferences.com/)
+
+
+# Education
+
+2013 - 2016
+:   *Computer Programming Student* [42 - Paris](https://42.fr)
+
+    Self-taught imperative programming with C, web development with PHP,
+    object-oriented development with C++, functional programming with OCaml.
+    Coursework includes Algorithms and Data Structures, UNIX Systems Programming.
+    Achieved highest possible grade (100% + bonuses) in implementation of
+    POSIX-compliant shell (4 students, 1 month).
+
+END
